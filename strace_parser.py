926aa531b866566f2fe52e32dcbad134d3bb8fc4
Johan Herland <johan@herland.net>
2015-06-27 22:55:28 +0200

    strace_parser.py: Refactor mercilessly

--- strace_parser.py
+++ strace_parser.py
@@ -1,7 +1,12 @@
 import logging
 import re
 
-Const = {
+
+class StraceParseError(NotImplementedError):
+    pass
+
+
+_Const = {
     'F_OK': 0x0001,
     'R_OK': 0x0002,
     'O_RDONLY': 0x0010,
@@ -13,91 +18,114 @@ Const = {
     'O_DIRECTORY': 0,
 }
 
-def parse_args(args):
+
+def _parse_args(args):
     # The following hack is the most compelling reason to rewrite this with
     # our own preloaded library mixin, instead of using strace
-    return eval(args, {}, Const)
+    return eval(args, {}, _Const) # Hmm, it sorta looks like valid python...
 
 
-def parse_strace_output(f):
+def _handle_exec(func, args, ret, rest):
+    executable, argv, env_s = _parse_args(args)
+    assert func == 'execve' and ret == 0 and not rest
+    return 'exec', (executable, argv, dict(s.split('=', 1) for s in env_s))
+
+
+def _handle_access(func, args, ret, rest):
+    path, mode = _parse_args(args)
+    assert mode in (_Const['F_OK'], _Const['R_OK'])
+    assert ret == -1 and rest.startswith('ENOENT ')
+    return 'check', (path, False)
+
+
+def _handle_open(func, args, ret, rest):
+    if func == 'openat':
+        base, path, mode = _parse_args(args)
+        assert base == _Const['AT_FDCWD']
+    else:
+        path, mode = _parse_args(args)
+    if ret == -1:
+        assert mode & _Const['O_RDONLY']
+        assert rest.startswith('ENOENT ')
+        return 'check', (path, False)
+    elif mode & _Const['O_RDONLY']:
+        assert ret > 0 and not rest
+        return 'read', (path,)
+    else:
+        raise NotImplementedError
+
+
+def _handle_stat(func, args, ret, rest):
+    path, struct = _parse_args(args)
+    if ret == 0:
+        assert not rest
+    else:
+        assert ret == -1 and rest.startswith('ENOENT ')
+    return 'check', (path, ret == 0)
+
+
+def _handle_readlink(func, args, ret, rest):
+    path, target, bufsize = _parse_args(args)
+    if ret > 0:
+        assert not rest
+        return 'read', (path,)
+    else:
+        assert ret == -1
+        if rest.startswith('ENOENT '):
+            return 'check', (path, False)
+        elif rest.startswith('EINVAL '):
+            raise NotImplementedError
+        else:
+            raise NotImplementedError
+
+
+_func_handlers = {
+    'execve': _handle_exec,
+    'access': _handle_access,
+    'open': _handle_open,
+    'openat': _handle_open,
+    'stat': _handle_stat,
+    'lstat': _handle_stat,
+    'readlink': _handle_readlink,
+}
+
+
+def strace_output_events(f):
+    """Parse strace output from f into (pid, event, (args...)) tuples.
+
+    Possible events are (args in parentheses):
+        - 'exec' (executable, argv_list, env_dict)
+        - 'exit' (exit_code)
+        - 'read' (path)
+        - 'write' (path)
+        - 'check' (path, exists)
+    """
+
     syscall_pattern = re.compile(r'^(\d+) +(\w+)\((.*)\) += (-?\d+)(.*)$')
     exit_pattern = re.compile(r'^(\d+) +\+\+\+ exited with (\d+) \+\+\+$')
+
     for line in f:
         m = syscall_pattern.match(line)
         if m:
             try:
                 pid, func, args, ret, rest = m.groups()
-                yield int(pid), func, parse_args(args), int(ret), rest.strip()
+                event, details = _func_handlers[func](
+                    func, args, int(ret), rest.strip())
+                yield int(pid), event, details
             except:
-                logging.error('Failed to parse line: ' + line)
-                raise
+                raise StraceParseError(line)
         else:
             m = exit_pattern.match(line)
             if m:
                 pid, exit_code = m.groups()
-                yield int(pid), 'exit', int(exit_code), int(exit_code), None
+                yield int(pid), 'exit', (int(exit_code),)
             else:
-                logging.warning('Skipping unknown line: ' + line)
-
-
-def events(parsed_strace_output):
-    for pid, func, args, ret, rest in parsed_strace_output:
-        if func == 'execve':
-            prog, argv, env = args
-            yield pid, 'start', prog, argv, env, ret
-        elif func == 'exit':
-             yield pid, 'exit', ret
-        elif func == 'access':
-            path, mode = args
-            assert mode in (Const['F_OK'], Const['R_OK'])
-            assert ret == -1 and rest.startswith('ENOENT ')
-            yield pid, 'path', path, 'missing', func
-        elif func in ('open', 'openat'):
-            if func == 'openat':
-                base, path, mode = args
-                assert base == Const['AT_FDCWD']
-            else:
-                path, mode = args
-            verb = '???'
-            if mode & Const['O_RDONLY']:
-                verb = 'read'
-            if ret == -1:
-                assert rest.startswith('ENOENT ')
-                verb = 'missing'
-            else:
-                assert ret > 0 and not rest
-            yield pid, 'path', path, verb, func
-        elif func in ('stat', 'lstat'):
-            path, struct = args
-            if ret == 0:
-                assert not rest
-                verb = 'read'
-            else:
-                assert ret == -1 and rest.startswith('ENOENT ')
-                verb = 'missing'
-            yield pid, 'path', path, verb, func
-        elif func == 'readlink':
-            path, target, bufsize = args
-            if ret > 0:
-                assert not rest
-                verb = 'read'
-            else:
-                assert ret == -1
-                if rest.startswith('ENOENT '):
-                    verb = 'missing'
-                elif rest.startswith('EINVAL '):
-                    verb = 'read?'
-                else:
-                    logging.error('Failed to parse readline() retval: ' + rest)
-            yield pid, 'path', path, verb, func
-        else:
-            logging.error('Cannot create event for {!r}'.format(
-                (pid, func, args, ret, rest)))
+                raise StraceParseError(line)
 
 
 if __name__ == '__main__':
     from pprint import pprint
     import sys
 
-    for e in events(parse_strace_output(sys.stdin)):
+    for e in strace_output_events(sys.stdin):
         pprint(e, width=160)
