2b76a865581b80db565ec7e28761cd59986d7608
Johan Herland <johan@herland.net>
2015-06-27 23:00:57 +0200

    Collect trace events into a Process objects and display it

--- depfinder.py
+++ depfinder.py
@@ -7,7 +7,7 @@ import subprocess
 import sys
 from tempfile import TemporaryDirectory
 
-import strace_parser
+from strace_parser import strace_output_events
 
 
 @contextmanager
@@ -32,13 +32,60 @@ def start_trace(cmd_args, trace_output):
     return subprocess.Popen(args + cmd_args)
 
 
-def main(cmd_args):
+class Process:
+    """Summarize interesting observations about a running process."""
+
+    @classmethod
+    def from_events(cls, events):
+        p = None
+        for pid, event, args in events:
+            if p is None:
+                p = Process(pid, 0)
+                assert event == 'exec'
+            getattr(p, event)(*args)
+        return p
+
+    def __init__(self, pid, ppid=0):
+        self.pid = pid
+        self.ppid = ppid # Parent PID, 0 means unknown/uninteresting
+        self.executable = None
+        self.argv = None
+        self.env = None
+        self.paths_read = set() # Paths read by this process
+        self.paths_written = set() # Paths written by this process
+        self.paths_checked = set() # Paths whose (non-)existence was checked
+        self.exit_code = None
+
+    def exec(self, executable, argv, env):
+        self.executable = executable
+        self.argv = argv
+        self.env = env
+
+    def read(self, path):
+        self.paths_read.add(path)
+
+    def write(self, path):
+        self.paths_written.add(path)
+
+    def check(self, path, exists):
+        self.paths_checked.add((path, exists))
+
+    def exit(self, exit_code):
+        self.exit_code = exit_code
+
+
+def run_trace(cmd_args):
     with temp_fifo() as fifo:
         with start_trace(cmd_args, fifo) as trace:
             with open(fifo) as f:
-                for e in strace_parser.events(strace_parser.parse_strace_output(f)):
-                    pprint(e, width=160)
-    return trace.returncode
+                p = Process.from_events(strace_output_events(f))
+    assert p.exit_code == trace.returncode
+    return p
+
+
+def main(cmd_args):
+    p = run_trace(cmd_args)
+    pprint(p.__dict__, width=160)
 
 
 if __name__ == '__main__':
