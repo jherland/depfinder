1b0366c0acd7ed73b642c45dc6a2aa1270ecd2a3
Johan Herland <johan@herland.net>
2015-06-29 02:07:30 +0200

    Relocate and rename Process class to a separate module

--- depfinder.py
+++ depfinder.py
@@ -1,58 +1,17 @@
 #!/usr/bin/env python3
 
 from pprint import pprint
-import sys
 
+from process_trace import ProcessTrace
 from strace_helper import run_trace
 
 
-class Process:
-    """Summarize interesting observations about a running process."""
-
-    @classmethod
-    def from_events(cls, events):
-        p = None
-        for pid, event, args in events:
-            if p is None:
-                p = Process(pid, 0)
-                assert event == 'exec'
-            getattr(p, event)(*args)
-        return p
-
-    def __init__(self, pid, ppid=0):
-        self.pid = pid
-        self.ppid = ppid  # Parent PID, 0 means unknown/uninteresting
-        self.executable = None
-        self.argv = None
-        self.env = None
-        self.paths_read = set()  # Paths read by this process
-        self.paths_written = set()  # Paths written by this process
-        self.paths_checked = set()  # Paths whose (non-)existence was checked
-        self.exit_code = None
-
-    def exec(self, executable, argv, env):
-        self.executable = executable
-        self.argv = argv
-        self.env = env
-
-    def read(self, path):
-        self.paths_read.add(path)
-
-    def write(self, path):
-        self.paths_written.add(path)
-
-    def check(self, path, exists):
-        self.paths_checked.add((path, exists))
-
-    def exit(self, exit_code):
-        self.exit_code = exit_code
-
-
 def main(cmd_args):
-    p = Process.from_events(run_trace(cmd_args))
+    p = ProcessTrace.from_events(run_trace(cmd_args))
     pprint(p.__dict__, width=160)
     return p.exit_code
 
 
 if __name__ == '__main__':
+    import sys
     sys.exit(main(sys.argv[1:]))
---
+++ process_trace.py
@@ -0,0 +1,44 @@
+from pathlib import Path
+
+
+class ProcessTrace:
+    '''Summarize trace events from a process.'''
+
+    @classmethod
+    def from_events(cls, events):
+        p = None
+        for pid, event, args in events:
+            if p is None:
+                p = cls(pid, 0)
+                assert event == 'exec'
+            getattr(p, event)(*args)
+        return p
+
+    def __init__(self, pid, ppid=0, cwd=None):
+        self.pid = pid
+        self.ppid = ppid  # Parent PID, 0 means unknown/uninteresting
+        self.cwd = cwd or Path.cwd()
+        self.executable = None
+        self.argv = None
+        self.env = None
+        self.paths_read = set()  # Paths read by this process
+        self.paths_written = set()  # Paths written by this process
+        self.paths_checked = set()  # Paths whose (non-)existence was checked
+        self.exit_code = None
+
+    def exec(self, executable, argv, env):
+        self.executable = self.cwd / executable
+        self.argv = argv
+        self.env = env
+
+    def read(self, path):
+        self.paths_read.add((path, self.cwd / path))
+
+    def write(self, path):
+        self.paths_written.add((path, self.cwd / path))
+
+    def check(self, path, exists):
+        self.paths_checked.add((path, self.cwd / path, exists))
+
+    def exit(self, exit_code):
+        self.exit_code = exit_code
